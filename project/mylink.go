1a14838a46431baf1dd96b32c7bd8aef3acdcd76
Makarova <makolika@gmail.com>
2018-02-09 16:45:46 +0500

    first commit

---
+++ project/mylink.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/go-martini/martini"
+	"net/http"
+	"loctalk/conf"
+	"loctalk/controllers"
+	"loctalk/models"
+	"loctalk/utils"
+)
+
+func main() {
+	m := martini.Classic()
+}
